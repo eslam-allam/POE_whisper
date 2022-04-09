62268f9463cae834da537766300b8e2cb26d9cf7
eslam-allam <eslamallam73@gmail.com>
2022-04-09 17:58:09 +0300

    Update POE_Whisper.py
    program now waits for key press before terminating in case of exception. added more exception messages. program retries when log file is missing or cant be accessed.

--- POE_Whisper.py
+++ POE_Whisper.py
@@ -1,22 +1,22 @@
 import logging
-from pickle import FALSE
 from watchfiles import run_process
 import requests
 import os
 import threading
 import time
 import sys
+import msvcrt as m
+from watchfiles import Change, DefaultFilter
 
 logging.getLogger("watchfiles").setLevel(logging.CRITICAL)
 
 mylogs = logging.getLogger(__name__)
 mylogs.setLevel(logging.DEBUG)
 
-file = logging.FileHandler("program_logs.log")
+file = logging.FileHandler("program_logs.log",encoding='utf-8')
 file.setLevel(logging.INFO)
 fileformat = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
 file.setFormatter(fileformat)
-
 mylogs.addHandler(file)
 
 stream = logging.StreamHandler()
@@ -26,7 +26,6 @@ stream.setFormatter(streamformat)
 
 
 mylogs.addHandler(stream)
-
 
 mode = 0o777
 flags = os.O_RDONLY
@@ -38,25 +37,15 @@ telegram_bot_token = ''
 telegram_chatID = ''
 kill_thread = False
 
-def refresh():
-    global kill_thread
-    attempts = 0
-    while True:
-        if kill_thread == True:
-            mylogs.info('KILLING THREAD')
-            sys.exit()
-        
-        try:
-            file= os.open(client_log_file, flags, mode)
-            os.close(file)
-            time.sleep(1)
-        except Exception as e:
-            mylogs.warning('ACCESS DENIED! REATTEMPTING')
-            attempts += 1
-            if attempts == 3:
-                mylogs.error('TOO MANY ATTEMPTS! TERMINATING!!')
-                sys.exit()
-        
+
+def wait():
+        m.getch()
+
+class textFilter(DefaultFilter):
+    allowed_extensions = '.txt'
+
+    def __call__(self, change: Change, path: str) -> bool:
+        return super().__call__(change, path) and path.endswith(self.allowed_extensions)
 
 def startup():
 
@@ -96,7 +85,7 @@ def startup():
         with open(client_log_file, 'r', encoding='utf-8') as f:
             count = f.readlines()
     except FileNotFoundError:
-        mylogs.error('COULD NOT LOCATE LOG FILE')  
+        mylogs.error('COULD NOT LOCATE LOG FILE ---- MAKE SURE LOG FILE PATH IS SET CORRECTLY E.G...../logs/Client.txt')  
         sys.exit(0)
 
     length = len(count)
@@ -105,76 +94,93 @@ def startup():
     with open(program_count_file,'w+',encoding='utf-8') as f:
             f.write(str(length))
 
-from watchfiles import Change, DefaultFilter
-
-
-class textFilter(DefaultFilter):
-    allowed_extensions = '.txt'
-
-    def __call__(self, change: Change, path: str) -> bool:
-        return super().__call__(change, path) and path.endswith(self.allowed_extensions)
-
+def refresh():
+    mylogs.info('THREAD STARTED')
+    denied = False
+    global kill_thread
+    while not kill_thread:
+        try:
+            file= os.open(client_log_file, flags, mode)
+            if denied == True:
+                mylogs.info('ACCESS RECOVERED!')
+                denied = False
+            os.close(file)
+            time.sleep(1)
+        except Exception:
+                mylogs.warning('ACCESS TO LOGS DENIED ----- RETRYING ----- PRESS (CTRL + C) TO END')
+                denied = True
+                time.sleep(5)
+    mylogs.info('THREAD CLOSED')
+    sys.exit(0)
 
 
 def  whisper(client_log_file, program_count_file, telegram_bot_token, telegram_chatID):
-    with open(program_count_file,'r',encoding='utf-8') as f:
-        last_length = int(f.readline())
-    content = ''
-    whispers = ''
-    with open(client_log_file,'r',encoding='utf-8') as f:
-        lines = f.readlines()
-        
-        if lines:
+    global th
+    try:
+        with open(program_count_file,'r',encoding='utf-8') as f:
+            last_length = int(f.readline())
+        content = ''
+        whispers = ''
+        with open(client_log_file,'r',encoding='utf-8') as f:
+            lines = f.readlines()
             
-            if last_length != 0:
-                if last_length != len(lines):
-                    diffirence = len(lines) - last_length
-                    content = lines[-diffirence:]
-                    
-            else: 
-                last_length = len(lines)
-                if lines:
-                    content = lines[-10:]
-                    
+            if lines:
+                
+                if last_length != 0:
+                    if last_length != len(lines):
+                        diffirence = len(lines) - last_length
+                        content = lines[-diffirence:]
+                        
+                else: 
+                    last_length = len(lines)
+                    if lines:
+                        content = lines[-10:]
+                        
+                
+            if lines: last_length = len(lines)
+        if content:
+            for line in content:
+                if '@From' in line:
+                    whispers += line[line.find('@From'):] + '\n'
             
-        if lines: last_length = len(lines)
-    if content:
-        for line in content:
-            if '@From' in line:
-                whispers += line[line.find('@From'):] + '\n'
-        
-        if whispers:
-            response = requests.get('https://api.telegram.org/bot{}/sendMessage'.format(telegram_bot_token),params={'text':whispers,'chat_id': '{}'.format(telegram_chatID)})
-            status = response.json()
-            if status['ok']:
-                mylogs.info('**********************STATUS:OK**********************')
-                sender = status['result']['from']
-                chat = status['result']['chat']
-                mylogs.info('MESSAGE SENT THROUGH {} TO {} {}'.format(sender['first_name'],chat['first_name'], chat['last_name']))
-                mylogs.info('MESSAGE: {}'.format(whispers))
-            else:
-                mylogs.warning('**********************STATUS:NO OK**********************')
-                mylogs.warning('MESSAGE NOT SENT!! PLEASE CHECK BOT PARAMETERS OR CHAT ID')
+            if whispers:
+                response = requests.get('https://api.telegram.org/bot{}/sendMessage'.format(telegram_bot_token),params={'text':whispers,'chat_id': '{}'.format(telegram_chatID)})
+                status = response.json()
+                if status['ok']:
+                    mylogs.info('**********************STATUS:OK**********************')
+                    sender = status['result']['from']
+                    chat = status['result']['chat']
+                    mylogs.info('MESSAGE SENT THROUGH {} TO {} {}'.format(sender['first_name'],chat['first_name'], chat['last_name']))
+                    mylogs.info('MESSAGE: {}'.format(whispers))
+                else:
+                    mylogs.warning('**********************STATUS:NO OK**********************')
+                    mylogs.warning('MESSAGE NOT SENT!! PLEASE CHECK BOT PARAMETERS OR CHAT ID')
 
-    with open(program_count_file,'w',encoding='utf-8') as f:
-        f.write(str(last_length))
+        with open(program_count_file,'w',encoding='utf-8') as f:
+            f.write(str(last_length))
+    except Exception:
+        mylogs.error('UNABLE TO ACCESS CLIENT LOG!!')
+
+def catch(arg):
+    for change in arg:
+        if str(change[0]) == 'Change.deleted':
+            mylogs.warning('{} HAS BEEN DELETED!'.format(change[1]))
+        
+
 
 def main():
-    mylogs.info('PROGRAM RUNNING')
-    run_process(client_log_folder, target=whisper, watch_filter=textFilter(), args=(client_log_file, program_count_file, telegram_bot_token, telegram_chatID))
-
+    mylogs.info('PROGRAM RUNNING --------- PRESS (CTRL + C) TO EXIT')
+    run_process(client_log_folder, target=whisper, watch_filter=textFilter(), args=(client_log_file, program_count_file, telegram_bot_token, telegram_chatID), callback=catch)
 
 if __name__ == '__main__':
     
     try:
-        th = threading.Thread(target=refresh, daemon=False)
+        th = threading.Thread(target=refresh, daemon=False, name='refreshThread')
         mylogs.info('LOADING SETUP PREFERENCES')
         startup()
         mylogs.info('PREFERENCES LOADED SUCCESSFULLY')
         mylogs.info('STARTING REFRESH THREAD')
         th.start()
-        mylogs.info('THREAD STARTED')
-
         main()
         
         
@@ -199,5 +205,8 @@ if __name__ == '__main__':
         if th: 
             if th.is_alive():
                 th.join()
+        
+        mylogs.info('PROGRAM ABOUT TO CLOSE - PRESS ANY KEY TO CONTINUE')
+        wait()
         mylogs.info('PROGRAM TERMINATED')
         
